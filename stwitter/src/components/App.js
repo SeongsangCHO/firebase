eea6969bc4796db073fc598a2bc86f865b967b24
seongsangcho <liasqui31@google.com>
2021-02-15 20:49:09 +0900

    add: error handling

--- stwitter/src/components/App.js
+++ stwitter/src/components/App.js
@@ -1,15 +1,26 @@
 import AppRouter from "components/Router";
-import { React, useState } from "react";
-import firebase from "../fbase";
+import { React, useState, useEffect } from "react";
+import { authService } from 'fbase';
 
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    //유저 상태를 추적하는 이벤트리스너를 마운트될 때 등록. 변경되면 콜백 수행
+    authService.onAuthStateChanged((user) => {
+      if (user){
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
   <>
-    <AppRouter isLoggedIn={isLoggedIn} />
-    <footer>&copy;  {new Date().getFullYear()} Switter</footer>
+    {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initialized..."}
+    <footer>&copy; {new Date().getFullYear()} Switter</footer>
   </> );
 }
 
--- stwitter/src/routes/Auth.js
+++ stwitter/src/routes/Auth.js
@@ -1,10 +1,66 @@
-import { React } from 'react';
+import { authService } from "fbase";
+import { React, useState } from "react";
 // import './Auth.scss'
 
 const Auth = () => {
+  const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(email, password);
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+      console.log(error);
+    }
+  };
   return (
-    <span>Auth</span>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+        ></input>
+        <input
+          onChange={onChange}
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+          value={password}
+        ></input>
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Login"}
+        ></input>
+        {error}
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
   );
-}
+};
 
 export default Auth;
