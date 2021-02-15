8c0a1ba2d3b38a5f6d0b1747c4d6aa5ae5b91406
seongsangcho <liasqui31@google.com>
2021-02-15 15:53:35 +0900

    init project

---
+++ stwitter/src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAxn5LR4dG_pEodeeXARdNHPSIGaeCdSMs",
+  authDomain: "stwitter-87ef6.firebaseapp.com",
+  projectId: "stwitter-87ef6",
+  storageBucket: "stwitter-87ef6.appspot.com",
+  messagingSenderId: "1081009089133",
+  appId: "1:1081009089133:web:c1e42d2021573157616d64"
+};
+
+export default firebase.initializeApp(firebaseConfig);
